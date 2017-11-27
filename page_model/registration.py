823090046d54fa623b19acb5cc018f22e8b5dc4b
Aadilah Aziz <aadilahaziz33@gmail.com>
2017-11-27 17:40:58 +0700

    final registration model & test case

--- page_model/registration.py
+++ page_model/registration.py
@@ -5,16 +5,21 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import pytest
+import time
+
+from page_model import Login
 
 class Registration(object):
 
     field_company_name_id = "BusinessName"
+    dropdown_salutation = "html/body/div[1]/div[3]/div/div[2]/div[1]/form/div[2]/span/span"
+    dropdown_salutation_mr = "//*[@id='Title_listbox']/li[2]"
     field_first_name_id = "FirstName"
     field_last_name_id = "LastName"
     field_email_address_id = "EmailAddress"
     field_password_id = "Password"
     field_repeat_password_id = "RepeatPassword"
-    button_tnc = "html/body/div[1]/div[3]/div[2]/div[2]/div[1]/form/div[7]/label/span"
+    button_tnc = "/html/body/div[1]/div[3]/div[2]/div[2]/div[1]/form/div[7]/label"
     button_create_id = "button-submit"
 
     def __init__(self, driver):
@@ -23,6 +28,8 @@ class Registration(object):
     def verify_all_element(self):
         try:
             self.driver.find_element_by_id(self.field_company_name_id)
+            self.driver.find_element_by_xpath(self.dropdown_salutation)
+            self.driver.find_element_by_xpath(self.dropdown_salutation_mr)
             self.driver.find_element_by_id(self.field_first_name_id)
             self.driver.find_element_by_id(self.field_last_name_id)
             self.driver.find_element_by_id(self.field_email_address_id)
@@ -39,6 +46,14 @@ class Registration(object):
         company_el.clear()
         company_el.send_keys(company_name)
         print("input company name")
+
+    def select_salutation(self):
+        salutation_el = self.driver.find_element_by_xpath(self.dropdown_salutation)
+        salutation_el.click()
+        time.sleep(1)
+        mr_el = self.driver.find_element_by_xpath(self.dropdown_salutation_mr)
+        mr_el.click()
+        print("select salutation")
 
     def input_first_name(self, first_name):
         fname_el = self.driver.find_element_by_id(self.field_first_name_id)
@@ -71,20 +86,120 @@ class Registration(object):
         print("input repeat password")
 
     def tick_tnc(self):
-        tnc_el = self.driver.find_element_by_id(self.button_tnc)
+        tnc_el = self.driver.find_element_by_xpath(self.button_tnc)
         tnc_el.click()
         print("tick tnc")
 
     def create_account(self):
-        create_el = self.driver.find_element_by_xpath(self.button_create_id)
+        create_el = self.driver.find_element_by_id(self.button_create_id)
         create_el.click()
         print("click create")
 
     def is_registration_success(self):
         try:
             WebDriverWait(self.driver, 30).until(
-                EC.presence_of_element_located((By.XPATH, "html/body/div[1]/div/div[1]/form/div[5]/div/button")))
+                EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'Log in')]")))
             print("Registration success")
         except TimeoutException:
             self.driver.get_screenshot_as_file("D:\\works\\hub3c_selenium\\log_test\\registration_failed.png")
             pytest.fail("Registration Failed")
+
+    def is_company_name_exist(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, "//*[contains(text(), 'Company Name or Family Name already exists.')]")))
+            print("Company name is already exist")
+        except TimeoutException:
+            self.driver.get_screenshot_as_file("D:\\works\\hub3c_selenium\\log_test\\registration_failed.png")
+            pytest.fail("Warning was not displayed")
+
+    def is_email_address_exist(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Email Address you entered already exists.')]")))
+            print("Email address is already exist")
+        except TimeoutException:
+            self.driver.get_screenshot_as_file("D:\\works\\hub3c_selenium\\log_test\\registration_failed.png")
+            pytest.fail("Warning was not displayed")
+
+    def is_company_name_empty(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Business name required')]")))
+            print("Company name is empty")
+        except TimeoutException:
+            self.driver.get_screenshot_as_file("D:\\works\\hub3c_selenium\\log_test\\registration_failed.png")
+            pytest.fail("Warning was not displayed")
+
+    def is_first_name_empty(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'First Name required')]")))
+            print("First name is empty")
+        except TimeoutException:
+            self.driver.get_screenshot_as_file("D:\\works\\hub3c_selenium\\log_test\\registration_failed.png")
+            pytest.fail("Warning was not displayed")
+
+    def is_last_name_empty(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Last Name required')]")))
+            print("Last name is empty")
+        except TimeoutException:
+            self.driver.get_screenshot_as_file("D:\\works\\hub3c_selenium\\log_test\\registration_failed.png")
+            pytest.fail("Warning was not displayed")
+
+    def is_email_address_empty(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Email required')]")))
+            print("Email address is empty")
+        except TimeoutException:
+            self.driver.get_screenshot_as_file("D:\\works\\hub3c_selenium\\log_test\\registration_failed.png")
+            pytest.fail("Warning was not displayed")
+
+    def is_password_empty(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Password required')]")))
+            print("Password is empty")
+        except TimeoutException:
+            self.driver.get_screenshot_as_file("D:\\works\\hub3c_selenium\\log_test\\registration_failed.png")
+            pytest.fail("Warning was not displayed")
+
+    def is_repeat_password_empty(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Repeat Password required')]")))
+            print("Repeat password is empty")
+        except TimeoutException:
+            self.driver.get_screenshot_as_file("D:\\works\\hub3c_selenium\\log_test\\registration_failed.png")
+            pytest.fail("Warning was not displayed")
+
+    def is_password_unmatched(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Password did not match.')]")))
+            print("Password is unmatched")
+        except TimeoutException:
+            self.driver.get_screenshot_as_file("D:\\works\\hub3c_selenium\\log_test\\registration_failed.png")
+            pytest.fail("Warning was not displayed")
+
+    def is_password_invalid(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Password must contain at least 6-20 characters')]")))
+            print("Password is invalid")
+        except TimeoutException:
+            self.driver.get_screenshot_as_file("D:\\works\\hub3c_selenium\\log_test\\registration_failed.png")
+            pytest.fail("Warning was not displayed")
+
+    def is_email_address_invalid(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Email address is invalid.')]")))
+            print("Email address is invalid")
+        except TimeoutException:
+            self.driver.get_screenshot_as_file("D:\\works\\hub3c_selenium\\log_test\\registration_failed.png")
+            pytest.fail("Warning was not displayed")
--- test_cases/test_registration.py
+++ test_cases/test_registration.py
@@ -7,24 +7,243 @@ root = Path(__file__).parents[1]   #get the root directory
 root_model = str(root)
 sys.path.append(root_model)
 
-from page_model import Login
 from page_model import Registration
 from connection import Connection
+from faker import Factory
 
 driver = Connection.driver
-login = Login(driver)
 regis = Registration(driver)
 
 class TestRegistration():
 
+    fake = Factory.create()
+    first_name = "Auto"
+    last_name = fake.first_name()
+    last_name2 = fake.first_name()
+    company_name = "Auto " + last_name
+    company_name2 = "Auto " + last_name2
+    email = "auto." + last_name + "@mailinator.com"
+    email2 = "auto." + last_name2 + "@mailinator.com"
+    password = "ZXasqw12"
+
+    #1  Register by inputting all fields
     def test_registration(self):
         driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
-        regis.input_company_name("Automation-Hub")
-        regis.input_first_name("Auto")
-        regis.input_last_name("Hub")
-        regis.input_email_address("automation.hub1@mailinator.com")
-        regis.input_password("ZXasqw12")
-        regis.input_repeat_password("ZXasqw12")
+        regis.input_company_name(self.company_name)
+        regis.select_salutation()
+        regis.input_first_name(self.first_name)
+        regis.input_last_name(self.last_name)
+        regis.input_email_address(self.email)
+        regis.input_password(self.password)
+        regis.input_repeat_password(self.password)
         regis.tick_tnc()
         regis.create_account()
         regis.is_registration_success()
+
+    #2  Register by inputting all mandatory fields (w/o "Salutation")
+    def test_registration_mandatory_only(self):
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.input_company_name(self.company_name2)
+        regis.input_first_name(self.first_name)
+        regis.input_last_name(self.last_name2)
+        regis.input_email_address(self.email2)
+        regis.input_password(self.password)
+        regis.input_repeat_password(self.password)
+        regis.tick_tnc()
+        regis.create_account()
+        regis.is_registration_success()
+
+    #3  Register using existing Company name
+    def test_registration_existing_company_name(self):
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.input_company_name("Hub3c")
+        regis.input_first_name("Auto")
+        regis.input_last_name("Hub999")
+        regis.input_email_address("automation.hub999@mailinator.com")
+        regis.input_password(self.password)
+        regis.input_repeat_password(self.password)
+        regis.tick_tnc()
+        regis.create_account()
+        regis.is_company_name_exist()
+
+    #4  Register using existing email address
+    def test_registration_existing_email_address(self):
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.input_company_name("Automation-Hub999")
+        regis.input_first_name("Auto")
+        regis.input_last_name("Hub999")
+        regis.input_email_address("mike@hub3c.com")
+        regis.input_password(self.password)
+        regis.input_repeat_password(self.password)
+        regis.tick_tnc()
+        regis.create_account()
+        regis.is_email_address_exist()
+
+    #5  Register by emptying all mandatory fields
+    def test_registration_all_mandatory_fields_empty(self):
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.tick_tnc()
+        regis.create_account()
+        regis.is_company_name_empty()
+        regis.is_first_name_empty()
+        regis.is_last_name_empty()
+        regis.is_email_address_empty()
+        regis.is_password_empty()
+        regis.is_repeat_password_empty()
+
+    #6  Register by emptying only "Company Name"
+    def test_registration_company_name_empty(self):
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.input_first_name(self.first_name)
+        regis.input_last_name(self.last_name)
+        regis.input_email_address(self.email)
+        regis.input_password(self.password)
+        regis.input_repeat_password(self.password)
+        regis.tick_tnc()
+        regis.create_account()
+        regis.is_company_name_empty()
+
+    #7  Register by emptying only "First Name"
+    def test_registration_first_name_empty(self):
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.input_company_name(self.company_name)
+        regis.input_last_name(self.last_name)
+        regis.input_email_address(self.email)
+        regis.input_password(self.password)
+        regis.input_repeat_password(self.password)
+        regis.tick_tnc()
+        regis.create_account()
+        regis.is_first_name_empty()
+
+    #8  Register by emptying only "Last Name"
+    def test_registration_last_name_empty(self):
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.input_company_name(self.company_name)
+        regis.input_first_name(self.first_name)
+        regis.input_email_address(self.email)
+        regis.input_password(self.password)
+        regis.input_repeat_password(self.password)
+        regis.tick_tnc()
+        regis.create_account()
+        regis.is_last_name_empty()
+
+    #9  Register by emptying only "Email"
+    def test_registration_email_address_empty(self):
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.input_company_name(self.company_name)
+        regis.input_first_name(self.first_name)
+        regis.input_last_name(self.last_name)
+        regis.input_password(self.password)
+        regis.input_repeat_password(self.password)
+        regis.tick_tnc()
+        regis.create_account()
+        regis.is_email_address_empty()
+
+    #10 Register by emptying only "Password"
+    def test_registration_password_empty(self):
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.input_company_name(self.company_name)
+        regis.input_first_name(self.first_name)
+        regis.input_last_name(self.last_name)
+        regis.input_email_address(self.email)
+        regis.input_repeat_password(self.password)
+        regis.tick_tnc()
+        regis.create_account()
+        regis.is_password_empty()
+
+    #11 Register by emptying only "Repeat Password"
+    def test_registration_repeat_password_empty(self):
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.input_company_name(self.company_name)
+        regis.input_first_name(self.first_name)
+        regis.input_last_name(self.last_name)
+        regis.input_email_address(self.email)
+        regis.input_password(self.password)
+        regis.tick_tnc()
+        regis.create_account()
+        regis.is_repeat_password_empty()
+
+    #12 Register by inputting unmatched password
+    def test_registration_unmatched_password(self):
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.input_company_name(self.company_name)
+        regis.input_first_name(self.first_name)
+        regis.input_last_name(self.last_name)
+        regis.input_email_address(self.email)
+        regis.input_password(self.password)
+        regis.input_repeat_password("ZXasqw123")
+        regis.tick_tnc()
+        regis.create_account()
+        regis.is_password_unmatched()
+
+    #13 Register by inputting invalid password: 6 chars, without one uppercase letter
+    def test_registration_password_without_uppercase(self):
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.input_company_name(self.company_name)
+        regis.input_first_name(self.first_name)
+        regis.input_last_name(self.last_name)
+        regis.input_email_address(self.email)
+        regis.input_password("zxas12")
+        regis.input_repeat_password("zxas12")
+        regis.tick_tnc()
+        regis.create_account()
+        regis.is_password_invalid()
+
+    #14 Register by inputting invalid password: 6 chars, without one lowercast letter
+    def test_registration_password_without_lowercase(self):
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.input_company_name(self.company_name)
+        regis.input_first_name(self.first_name)
+        regis.input_last_name(self.last_name)
+        regis.input_email_address(self.email)
+        regis.input_password("ZXAS12")
+        regis.input_repeat_password("ZXAS12")
+        regis.tick_tnc()
+        regis.create_account()
+        regis.is_password_invalid()
+
+    #15 Register by inputting invalid password: 6 chars, without one number
+    def test_registration_password_without_number(self):
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.input_company_name(self.company_name)
+        regis.input_first_name(self.first_name)
+        regis.input_last_name(self.last_name)
+        regis.input_email_address(self.email)
+        regis.input_password("ZXASQW")
+        regis.input_repeat_password("ZXASQW")
+        regis.tick_tnc()
+        regis.create_account()
+        regis.is_password_invalid()
+
+    #16 Register by inputting invalid password: <6 chars
+    def test_registration_password_less_than_six(self):
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.input_company_name(self.company_name)
+        regis.input_first_name(self.first_name)
+        regis.input_last_name(self.last_name)
+        regis.input_email_address(self.email)
+        regis.input_password("ZXas1")
+        regis.input_repeat_password("ZXas1")
+        regis.tick_tnc()
+        regis.create_account()
+        regis.is_password_invalid()
+
+    #17 Register by inputting incorrect email format
+    def test_registration_invalid_email_address(self):
+        #without @
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.input_company_name(self.company_name)
+        regis.input_first_name(self.first_name)
+        regis.input_last_name(self.last_name)
+        regis.input_email_address("automation.hub999mailinator.com")
+        regis.input_password(self.password)
+        regis.input_repeat_password(self.password)
+
+        #without dot (.) after @
+        driver.get("https://test-z5y5zwrh0g.hub3c.com/Join/Index")
+        regis.input_company_name(self.company_name)
+        regis.input_first_name(self.first_name)
+        regis.input_last_name(self.last_name)
+        regis.input_email_address("automation.hub999@mailinator")
+        regis.input_password(self.password)
+        regis.input_repeat_password(self.password)
